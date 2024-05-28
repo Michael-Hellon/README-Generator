f7d8bee24c31008d81c11788e232029069d9e48a
Michael Hellon <kansasboy308@gmail.com>
2024-05-28 18:05:12 -0500

    added code content Changes to be committed: modified:   LICENSE modified:   index.js modified:   utils/generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,105 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+import { generateMarkdown } from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter a Title for your Project. ",
+        name: "title", 
+      },
+      {
+        type: "input",
+        message: "Enter a brief description of what your project does.",
+        name: "description", 
+      },
+      {
+        type: "input",
+        message: "What was your motivation??",
+        name: "motivation", 
+      },
+      {
+        type: "input",
+        message: "Why did you build this project?",
+        name: "build", 
+      },
+      {
+        type: "input",
+        message: "What problem does it solve?",
+        name: "problem", 
+      },
+      {
+        type: "input",
+        message: "What did you learn?",
+        name: "learned", 
+      },
+      {
+        type: "input",
+        message: "What are the Installation Instructions?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What is the usage of this project?",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: 'What license type do you want to use for this project? (Use the up/down arrow keys to select, then press ENTER)',
+        name: 'license',
+        choices: [
+            "MIT license", 
+            "Apache License 2.0", 
+            "GNU General Public License v3.0",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ]
+      },
+      {
+        type: "input",
+        message: "How does someone contribute to this project?",
+        name: "contribute",
+      },
+      {
+        type: "input",
+        message: "what are the test conditions for this project?",
+        name: "test",
+      },
+      {
+        type: "input",
+        message: "Please enter your GitHub username.",
+        name: "userName",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please enter who need to be credited for the work on this project.",
+        name: "credits",
+      },    
+      {
+        type: "input",
+        message: "What is the FILE NAME for the screenshot?",
+        name: "screenshot",
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, questions) {
+    fs.writeFile("New_README.md", readMe, (err) =>
+        err ? console.log(colors.bgBrightRed(err)) : console.log(colors.bgBrightGreen("success!"))
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,105 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge(ICON) based on which license is passed in 
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    switch (license){
+        case "MIT license":
+            return "[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)]"; 
+        case "Apache License 2.0":
+            return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]"; 
+        case "GNU General Public License v3.0":
+            return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+        case "Mozilla Public License 2.0":
+            return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+        case "The Unlicense":
+            return "[![License](https://img.shields.io/badge/License-Unlicense-green.svg)]";
+        default:
+            return "";
+    }
 
-// TODO: Create a function that returns the license link
+}
+
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch (license){
+        case "MIT license":
+            return "[![License: MIT]](https://opensource.org/licenses/MIT)";
+        case "Apache License 2.0":
+            return "[![License]](https://opensource.org/licenses/Apache-2.0)";
+        case "GNU General Public License v3.0":
+            return "[![License: GPL v3]](https://opensource.org/license/gpl-3-0/)";
+        case "Mozilla Public License 2.0":
+            return "[![License: MPL 2.0]](https://opensource.org/licenses/MPL-2.0)";
+        case "The Unlicense":
+            return "[![License]](https://opensource.org/license/unlicense)";
+        default:
+            return ""
+    }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== undefined) {
+        return `Please click on the badge below to read information about the ${data.license} license.`;
+    } else{
+        return "";
+    } 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+        ${data.description}
+
+        - ${data.motivation}
+        - ${data.build}
+        - ${data.problem}
+        - ${data.learned}
+
+        ## Table of contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [Contributing](#contribute)
+        * [Tests](#test)
+        * [Questions](#question)
+        * [License](#license) 
+        
+    
+        ## Installation
+        ${data.installation}
+
+        ## Usage
+        ${data.usage}
+
+        Here is the link to watch the demo of the application </asset/video/demoVideo
+
+        ## License
+        ${data.license}
+
+        ## How to Contribute to this project
+        ${data.contribute}
+        
+        ## Tests
+        ${data.test}
+
+        ## Questions
+        My Github profile can be located at <https://github.com/${data.userName}/>
+
+        You can email me with any questions at ${data.email}
+
+        ![screenshot](/assets/images/${screenshot})
+
+        ## Credits
+        ${data.credits}
+        
+        ## License
+        ${renderLicenseSection}
+
+        ${renderLicenseBadge}
 `;
 }
 
